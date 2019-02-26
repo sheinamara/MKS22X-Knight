b1cd70563977c7f3505a81a869b01819b6dd24f3
sheinamara <ashein00@stuy.edu>
2019-02-25 22:46:28 -0500

    made the file

---
+++ KnightBoard.java
@@ -0,0 +1,12 @@
+public class KnightBoard{
+  /**
+  * Default constructor.
+  * @throws IllegalArgumentException when either parameter is <= 0.
+  */
+  public KnightBoard(int rows,int cols){
+  }
+
+  public String toString(){}
+
+
+}
